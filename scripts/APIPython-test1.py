413bf77da5ffa325698879a48c22493cbd57becd
Mathilde Christiaens <mathildechristiaens@hotmail.fr>
2020-01-14 16:13:53 +0100

    ajout scripts python et maj docker-compose

---
+++ scripts/APIPython-test1.py
@@ -0,0 +1,27 @@
+# create an OBjectStoreAPI object
+from oio import ObjectStorageApi
+s = ObjectStorageApi("OPENIO")
+
+#creer un compte
+from oio.account.client import AccountClient
+ac = AccountClient({"namespace": "OPENIO"})
+
+# Account creation: Returns true if everything went well.
+ac.account_create("my_ac2") 
+
+#create a container
+s.container_create(ac, "node1")
+
+#shows the description  of a container
+s.container_get_properties(ac,"node1")
+
+#create ano object .text
+data = "My data"
+s.object_create("my_ac2", "node1", obj_name="essai.txt",data=data)
+
+#retrieving object
+meta, stream = s.object_fetch("my_ac2","node1", "essai.txt")
+print "".join(stream)
+
+#deleting objects
+s.object_delete("my_ac2", "node1", "essai.txt")
