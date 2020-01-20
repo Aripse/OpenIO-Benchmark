0ad48ab5dcbc723cbbc46056e183468fd5f2dc36
NADEAU_Morgane <35223178+morganad@users.noreply.github.com>
2020-01-20 13:54:50 +0100

    Update uploadFolder.py

--- scripts/uploadFolder.py
+++ scripts/uploadFolder.py
@@ -4,23 +4,36 @@ from urllib.parse import unquote
 from oio import ObjectStorageApi
 from oio.account.client import AccountClient
 import os
+import argparse
 
-s = ObjectStorageApi("OPENIO")
+
+def uploadFolder(client, container, folder_path):
+    s = ObjectStorageApi("OPENIO")
+    for file_name_ext in os.listdir(folder_path):
+        file_path_ext=str(folder_path)+'/'+file_name_ext
+        file_name, file_extension = os.path.splitext(file_name_ext)
+        with open(file_path_ext, 'rb') as f:
+            data = f.read()
+            s.object_create(client, container, obj_name=file_name_ext, data=data)
+            meta, stream = s.object_fetch(client, container, file_name_ext)
+        with open('./newfile'+file_extension, 'w+b') as e:
+            e.write(b"".join(stream))
+
+
 ac = AccountClient({"namespace": "OPENIO"})
 
-#chemin du dossier
-folder_path='./To_save'
+parser = argparse.ArgumentParser(
+    description='Upload all files of a folder in the container you desire and for a specific account for a client.')
 
-#lister tous les fichier
+parser.add_argument('client', type=str, nargs='?',
+                    help='The name of the client you want to associate this file')
 
-for file_name_ext in os.listdir(folder_path):
-    file_path_ext=folder_path+'/'+file_name_ext
-    file_name, file_extension = os.path.splitext(file_name_ext)
-    with open(file_path_ext, 'rb') as f:
-    	data = f.read()
+parser.add_argument('container', type=str, nargs='?',
+                    help='The container you want to put the file in')
 
-    	s.object_create("my_ac1", "node1", obj_name=file_name_ext, data=data)
-    	meta, stream = s.object_fetch("my_ac1", "node1", file_name_ext)
+parser.add_argument('path', type=str, nargs='?',
+                    help='The path of the file you want to put inside the cluster')
 
-    with open('./newfile'+file_extension, 'w+b') as e:
-        e.write(b"".join(stream))
+args = parser.parse_args()
+
+uploadFolder(args.client, args.container, args.path)
