d9f0d4f1155c26d4a1a36fa4653ea290a8deea7a
NADEAU_Morgane <35223178+morganad@users.noreply.github.com>
2020-02-07 12:22:40 +0100

    add endpoint

--- scripts/uploadFolder.py
+++ scripts/uploadFolder.py
@@ -7,8 +7,9 @@ import os
 import argparse
 
 
-def uploadFolder(client, container, folder_path):
-    s = ObjectStorageApi("OPENIO")
+def uploadFolder(container, folder_path):
+    s = ObjectStorageApi("OPENIO", endpoint="http://169.254.205.203:6006")
+    client="admin"
     for file_name_ext in os.listdir(folder_path):
         file_path_ext=str(folder_path)+'/'+file_name_ext
         file_name, file_extension = os.path.splitext(file_name_ext)
@@ -16,17 +17,12 @@ def uploadFolder(client, container, folder_path):
             data = f.read()
             s.object_create(client, container, obj_name=file_name_ext, data=data)
             meta, stream = s.object_fetch(client, container, file_name_ext)
-        with open('./newfile'+file_extension, 'w+b') as e:
-            e.write(b"".join(stream))
+       # with open('./'+file_name_ext, 'w+b') as e:
+       #     e.write(b"".join(stream))
 
-
-ac = AccountClient({"namespace": "OPENIO"})
 
 parser = argparse.ArgumentParser(
     description='Upload all files of a folder in the container you desire and for a specific account for a client.')
-
-parser.add_argument('client', type=str, nargs='?',
-                    help='The name of the client you want to associate this file')
 
 parser.add_argument('container', type=str, nargs='?',
                     help='The container you want to put the file in')
@@ -36,4 +32,4 @@ parser.add_argument('path', type=str, nargs='?',
 
 args = parser.parse_args()
 
-uploadFolder(args.client, args.container, args.path)
+uploadFolder(args.container, args.path)
