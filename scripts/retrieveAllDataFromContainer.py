8cabbe39a45f22127536c98363254e8b519930e8
NADEAU_Morgane <35223178+morganad@users.noreply.github.com>
2020-02-07 12:21:53 +0100

    add endpoint

--- scripts/retrieveAllDataFromContainer.py
+++ scripts/retrieveAllDataFromContainer.py
@@ -4,8 +4,9 @@ import argparse
 import os
 
 
-def retrieveAllDataFromContainer(client, container):
-    s = ObjectStorageApi("OPENIO")
+def retrieveAllDataFromContainer(container):
+    s = ObjectStorageApi("OPENIO",endpoint="http://169.254.205.203:6006")
+    client="admin"
     # print(s.object_list(client, container)['objects'])
     for element in s.object_list(client, container)['objects']:
         meta, stream = s.object_fetch(client, container, element['name'])
@@ -13,17 +14,12 @@ def retrieveAllDataFromContainer(client, container):
         with open(element['name'], 'w+b') as e:
             e.write(b"".join(stream))
 
-ac = AccountClient({"namespace": "OPENIO"})
-
 parser = argparse.ArgumentParser(
     description='Retrieve all the files from a certain container and a certain client.')
-
-parser.add_argument('client', type=str, nargs='?',
-                    help='The name of the client')
 
 parser.add_argument('container', type=str, nargs='?',
                     help='The name of the container')
 
 args = parser.parse_args()
 
-retrieveAllDataFromContainer(args.client, args.container)
+retrieveAllDataFromContainer(args.container)
