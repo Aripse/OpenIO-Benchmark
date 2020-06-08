bee4429c90e5389b180b3123ca3820fca4778978
NADEAU_Morgane <35223178+morganad@users.noreply.github.com>
2020-06-08 22:46:48 +0200

    Add retention policy

--- scripts/fonctions.py
+++ scripts/fonctions.py
@@ -59,21 +59,36 @@ session = boto3.Session()
 s3_client = session.client(service_name='s3', aws_access_key_id=config['awsAccessKeyId'], aws_secret_access_key =config['awsSecretKey'] , endpoint_url=config["awsEndpointUrl"])
 
 #function to add a file in the container
-def addFileInContainer(container, path):
+def addFileInContainer(container, path, retention=0):
     fileName = os.path.basename(path)
 
     #try/except
     s3_client.upload_file(Filename= path, Bucket=container, Key= fileName)
+    if retention !=0 :
+    	s3_client.put_object_retention(Bucket=container, Key= fileName, 
+    	Retention={
+        	'Mode': 'GOVERNANCE',
+       		'RetainUntilDate': datetime.today()+timedelta(retention)
+    	}
+)
+
 
 #function to delete a file in the container
 def deleteFileInContainer(container, fileName):
     s3_client.delete_object(Bucket=container, Key= fileName)
 
 #function to copy an entire folder from sever to the OpenIO container
-def uploadFolder( container, folder_path):
+def uploadFolder( container, folder_path, retention=0):
     for file_name_ext in os.listdir(folder_path):
         file_path_ext=str(folder_path)+'/'+file_name_ext
         s3_client.upload_file(Filename= file_path_ext, Bucket= container, Key=file_name_ext)
+        if retention !=0 :
+                s3_client.put_object_retention(Bucket=container, Key=file_name_ext, 
+    		Retention={
+        		'Mode': 'GOVERNANCE',
+       			'RetainUntilDate': datetime.today()+timedelta(retention)
+    		}
+	)
 
 #function to list all data inside a container
 def listDataForAGivenPeriod( container, period):
@@ -95,7 +110,7 @@ def retrieveAllDataFromContainer(container):
         s3_client.download_file(Bucket=container, Key=element['Key'], Filename= element['Key'])
 
 #function to copy an entire folder from ElasticSearch to the OpenIO container
-def elasticUploadFolder(container, index):
+def elasticUploadFolder(container, index, retention=0):
     if os.name == 'posix':
         slash = "/" # for Linux and macOS
     else:
@@ -109,8 +124,30 @@ def elasticUploadFolder(container, index):
         # get _source data dict from document
         source_data = doc["_source"]
         s3_client.upload_file(Filename= source_data["file_name"],Bucket=container, Key= source_data["file_name"])
+        if retention !=0 :
+                s3_client.put_object_retention(Bucket=container, Key= source_data["file_name"], 
+    		Retention={
+        		'Mode': 'GOVERNANCE',
+       			'RetainUntilDate': datetime.today()+timedelta(retention)
+    		}
+	)
 
 #function to create a container
 def addContainer(container):
     s3_client.create_bucket(Bucket=container)
+
+#function to get the retention policy of an object
+def getRetention(container, filename):
+    bucket_retention = s3_client.get_object_retention(Bucket=container, Key= filename)
+    print(bucket_retention)
+
+#function to modify the retention policy of an object
+def putRetention(container, filename, retention):
+    bucket_retention = s3_client.put_object_retention(Bucket=container, Key= filename,
+    Retention={
+        'Mode': 'GOVERNANCE',
+        'RetainUntilDate': datetime.today()+timedelta(retention)
+    }
+
+)
 
