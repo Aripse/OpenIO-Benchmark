88074340c838ca268e98be1f0b40749b5ff4a69e
NADEAU_Morgane <35223178+morganad@users.noreply.github.com>
2020-06-13 20:12:44 +0200

    Add list buckets fonctions + modify print

--- scripts/main.py
+++ scripts/main.py
@@ -28,9 +28,9 @@ with open("./config.yaml", "r") as ymlfile:
     config = yaml.load(ymlfile,  Loader=yaml.FullLoader)
 
 #configuring command line arguments
-parser = argparse.ArgumentParser(description='call the function you want : add to add a file in the container, delete to delete a file in the container, copy to copy an entire folder in the container, list to list all data inside a container, retrieve to retrieve all data from a container')
+parser = argparse.ArgumentParser(description='call the function you want : add to add a file in the container, delete to delete a file in the container, copy to copy an entire folder in the container, list_objects to list all data inside a container, retrieve to retrieve all data from a container')
 
-#name of the function to call (add, delete, copy, list, retrieve, elastic, create_container) 
+#name of the function to call (add, delete, copy, list_objects, retrieve, elastic, create_container) 
 parser.add_argument('method',type=str, nargs='?')
 
 #name of the container
@@ -61,13 +61,13 @@ help="The number of days from today until this Object Lock Retention will expire
 args=parser.parse_args()
 
 #verification of the presence of the argument "container" and the configuration variable "AccountClientNamespace" then launching of the function according to the argument "method"
-if not args.container:
-	print("You must have a container parameter for the function to work smoothly.")
-elif not config['AccountClientNamespace']:
+if not config['AccountClientNamespace']:
 	print("The AccountClientNamespace attribute does not exist in the configuration file. Please add it.")
 else:
 	if(args.method == "add"):
-		if not args.path:
+		if not args.container:
+			print("The function AddFileInContainer needs a container argument to work. Please try again..")
+		elif not args.path:
 			print("The function AddFileInContainer needs a path argument to work. Please try again.")
 		else:
 			print("Running the function AddFileInContainer with the parameters below :")
@@ -83,7 +83,9 @@ else:
 				fonctions.addFileInContainer(args.container, args.path)
 
 	elif(args.method == "delete"): #delete a file in the container
-		if not args.filename:
+		if not args.container:
+			print("The function DeleteFileInContainer needs a container argument to work. Please try again.")
+		elif not args.filename:
 			print("The function DeleteFileInContainer needs a filename argument to work. Please try again.")
 		else:
 			print("Running the function DeleteFileInContainer with the parameters below :")
@@ -96,7 +98,9 @@ else:
 			fonctions.deleteFileInContainer(args.container, args.filename)
 
 	elif(args.method == "copy"): #copy an entire folder from sever to the OpenIO container
-		if not args.path:
+		if not args.container:
+			print("The function UploadFolder needs a container argument to work. Please try again.")
+		elif not args.path:
 			print("The function UploadFolder needs a path argument to work. Please try again.")
 		else:
 			print("Running the function UploadFolder with the parameters below :")
@@ -113,8 +117,19 @@ else:
 			else:
 				fonctions.uploadFolder(args.container, args.path)
 
-	elif(args.method == "list"): #list all data inside a container
-		if not args.period:
+	elif(args.method == "list_containers"): #list all containers
+		print("Running the function ListDataForAGivenPeriod with the parameters below :")
+		print(" - client : "+ config['client'])
+		print(" - period : "+ str(args.period))
+		print(" - namespace : "+ config['AccountClientNamespace'])
+		print(" - endpoint : "+ config['awsEndpointUrl'])
+		input("Press Enter to continue...")
+		fonctions.listBuckets()
+
+	elif(args.method == "list_objects"): #list all data inside a container
+		if not args.container:
+			print("The function ListDataForAGivenPeriod needs a container argument to work. Please try again.")
+		elif not args.period:
 			print("The function ListDataForAGivenPeriod needs a period argument to work. Please try again.")
 		else:
 			print("Running the function ListDataForAGivenPeriod with the parameters below :")
@@ -127,105 +142,148 @@ else:
 			fonctions.listDataForAGivenPeriod( args.container, args.period)
 
 	elif(args.method == "retrieve"): #retrieve all data from a container
-		print("Running the function RetrieveAllDataFromContainer with the parameters below :")
-		print(" - container : "+ args.container)
-		print(" - client : "+ config['client'])
-		print(" - namespace : "+ config['AccountClientNamespace'])
-		print(" - endpoint : "+ config['awsEndpointUrl'])
-		input("Press Enter to continue...")
-		fonctions.retrieveAllDataFromContainer( args.container)
+		if not args.container:
+			print("The function retrieveAllDataFromContainer needs a container argument to work. Please try again.")
+		else:
+			print("Running the function RetrieveAllDataFromContainer with the parameters below :")
+			print(" - container : "+ args.container)
+			print(" - client : "+ config['client'])
+			print(" - namespace : "+ config['AccountClientNamespace'])
+			print(" - endpoint : "+ config['awsEndpointUrl'])
+			input("Press Enter to continue...")
+			fonctions.retrieveAllDataFromContainer( args.container)
 
 	elif(args.method == "elastic"): #copy an entire folder from ElasticSearch to the OpenIO container
-		print("Running the function elasticUploadFolder with the parameters below :")
-		print(" - container : "+ args.container)
-		print(" - index : "+ args.index)
-		print(" - client : "+ config['client'])
-		if args.retention:
-				print(" - retention :"+ str(args.retention))
-		print(" - namespace : "+ config['AccountClientNamespace'])
-		print(" - endpoint : "+ config['awsEndpointUrl'])
-		print(" - elasticsearchDomain :" + config["elasticsearchDomain"])
-		print(" - elasticsearchPort :" + config["elasticsearchPort"])
-		input("Press Enter to continue...")
-		if args.retention:
-			fonctions.elasticUploadFolder(args.container, args.index, args.retention)
+		if not args.container:
+			print("The function elasticUploadFolder needs a container argument to work. Please try again.")
+		elif not args.index:
+			print("The function elasticUploadFolder needs an index argument to work. Please try again.")
 		else:
-			fonctions.elasticUploadFolder(args.container, args.index)
+			print("Running the function elasticUploadFolder with the parameters below :")
+			print(" - container : "+ args.container)
+			print(" - index : "+ args.index)
+			print(" - client : "+ config['client'])
+			if args.retention:
+					print(" - retention :"+ str(args.retention))
+			print(" - namespace : "+ config['AccountClientNamespace'])
+			print(" - endpoint : "+ config['awsEndpointUrl'])
+			print(" - elasticsearchDomain :" + config["elasticsearchDomain"])
+			print(" - elasticsearchPort :" + config["elasticsearchPort"])
+			input("Press Enter to continue...")
+			if args.retention:
+				fonctions.elasticUploadFolder(args.container, args.index, args.retention)
+			else:
+				fonctions.elasticUploadFolder(args.container, args.index)
 
 	elif(args.method=="create_container"): #create a container
-		print("Running the function addContainer with the parameters below :")
-		print(" - container : "+ args.container)
-		if args.ACL:
-				print(" - ACL :"+ args.ACL)
-		print(" - client : "+ config['client'])
-		print(" - namespace : "+ config['AccountClientNamespace'])
-		print(" - endpoint : "+ config['awsEndpointUrl'])
-		input("Press Enter to continue...")
-		if args.ACL:
-			fonctions.addContainer(args.container,args.ACL)
+		if not args.container:
+			print("The function addContainer needs a container argument to work. Please try again.")
 		else:
-			fonctions.addContainer(args.container)
+			print("Running the function addContainer with the parameters below :")
+			print(" - container : "+ args.container)
+			if args.ACL:
+					print(" - ACL :"+ args.ACL)
+			print(" - client : "+ config['client'])
+			print(" - namespace : "+ config['AccountClientNamespace'])
+			print(" - endpoint : "+ config['awsEndpointUrl'])
+			input("Press Enter to continue...")
+			if args.ACL:
+				fonctions.addContainer(args.container,args.ACL)
+			else:
+				fonctions.addContainer(args.container)
 
 	elif(args.method=="get_container_ACL"): #get the Access Control List of a container
-		print("Running the function GetBucketACL with the parameters below :")
-		print(" - container : "+ args.container)
-		print(" - client : "+ config['client'])
-		print(" - namespace : "+ config['AccountClientNamespace'])
-		print(" - endpoint : "+ config['awsEndpointUrl'])
-		input("Press Enter to continue...")
-		fonctions.getBucketACL(args.container)
+		if not args.container:
+			print("The function getBucketACL needs a container argument to work. Please try again.")
+		else:
+			print("Running the function getBucketACL with the parameters below :")
+			print(" - container : "+ args.container)
+			print(" - client : "+ config['client'])
+			print(" - namespace : "+ config['AccountClientNamespace'])
+			print(" - endpoint : "+ config['awsEndpointUrl'])
+			input("Press Enter to continue...")
+			fonctions.getBucketACL(args.container)
 
 	elif(args.method=="put_container_ACL"): #modify the Access Control List of a container
-		print("Running the function PutBucketACL with the parameters below :")
-		print(" - container : "+ args.container)
-		print(" - ACL : "+ args.ACL)
-		print(" - client : "+ config['client'])
-		print(" - namespace : "+ config['AccountClientNamespace'])
-		print(" - endpoint : "+ config['awsEndpointUrl'])
-		input("Press Enter to continue...")
-		fonctions.putBucketACL(args.container,args.ACL)
+		if not args.container:
+			print("The function putBucketACL needs a container argument to work. Please try again.")
+		elif not args.ACL:
+			print("The function putBucketACL needs an ACL argument to work. Please try again.")
+		else:
+			print("Running the function putBucketACL with the parameters below :")
+			print(" - container : "+ args.container)
+			print(" - ACL : "+ args.ACL)
+			print(" - client : "+ config['client'])
+			print(" - namespace : "+ config['AccountClientNamespace'])
+			print(" - endpoint : "+ config['awsEndpointUrl'])
+			input("Press Enter to continue...")
+			fonctions.putBucketACL(args.container,args.ACL)
 
 	elif(args.method=="get_file_ACL"): #get the Access Control List of a file
-		print("Running the function GetObjectACL with the parameters below :")
-		print(" - container : "+ args.container)
-		print(" - filename : "+ args.filename)
-		print(" - client : "+ config['client'])
-		print(" - namespace : "+ config['AccountClientNamespace'])
-		print(" - endpoint : "+ config['awsEndpointUrl'])
-		input("Press Enter to continue...")
-		fonctions.getObjectACL(args.container,args.filename)
+		if not args.container:
+			print("The function getObjectACL needs a container argument to work. Please try again.")
+		elif not args.filename:
+			print("The function getObjectACL needs a filename argument to work. Please try again.")
+		else:
+			print("Running the function getObjectACL with the parameters below :")
+			print(" - container : "+ args.container)
+			print(" - filename : "+ args.filename)
+			print(" - client : "+ config['client'])
+			print(" - namespace : "+ config['AccountClientNamespace'])
+			print(" - endpoint : "+ config['awsEndpointUrl'])
+			input("Press Enter to continue...")
+			fonctions.getObjectACL(args.container,args.filename)
 
 	elif(args.method=="put_file_ACL"): #modify the Access Control List of a file
-		print("Running the function PutObjectACL with the parameters below :")
-		print(" - container : "+ args.container)
-		print(" - filename : "+ args.filename)
-		print(" - ACL : "+ args.ACL)
-		print(" - client : "+ config['client'])
-		print(" - namespace : "+ config['AccountClientNamespace'])
-		print(" - endpoint : "+ config['awsEndpointUrl'])
-		input("Press Enter to continue...")
-		fonctions.putObjectACL(args.container,args.filename, args.ACL)
+		if not args.container:
+			print("The function putObjectACL needs a container argument to work. Please try again.")
+		elif not args.filename:
+			print("The function putObjectACL needs a filename argument to work. Please try again.")
+		elif not args.ACL:
+			print("The function putObjectACL needs an ACL argument to work. Please try again.")
+		else:
+			print("Running the function putObjectACL with the parameters below :")
+			print(" - container : "+ args.container)
+			print(" - filename : "+ args.filename)
+			print(" - ACL : "+ args.ACL)
+			print(" - client : "+ config['client'])
+			print(" - namespace : "+ config['AccountClientNamespace'])
+			print(" - endpoint : "+ config['awsEndpointUrl'])
+			input("Press Enter to continue...")
+			fonctions.putObjectACL(args.container,args.filename, args.ACL)
 
 	elif(args.method=="get_retention"): #get the retention policy of an object
-		print("Running the function GetRetention with the parameters below :")
-		print(" - container : "+ args.container)
-		print(" - client : "+ config['client'])
-		print(" - filename : "+ args.filename)
-		print(" - namespace : "+ config['AccountClientNamespace'])
-		print(" - endpoint : "+ config['awsEndpointUrl'])
-		input("Press Enter to continue...")
-		fonctions.getRetention(args.container,args.filename)
+		if not args.container:
+			print("The function getRetention needs a container argument to work. Please try again.")
+		elif not args.filename:
+			print("The function getRetention needs a filename argument to work. Please try again.")
+		else:
+			print("Running the function getRetention with the parameters below :")
+			print(" - container : "+ args.container)
+			print(" - client : "+ config['client'])
+			print(" - filename : "+ args.filename)
+			print(" - namespace : "+ config['AccountClientNamespace'])
+			print(" - endpoint : "+ config['awsEndpointUrl'])
+			input("Press Enter to continue...")
+			fonctions.getRetention(args.container,args.filename)
 
 	elif(args.method=="put_retention"): #modify the retention policy of an object
-		print("Running the function GetRetention with the parameters below :")
-		print(" - container : "+ args.container)
-		print(" - client : "+ config['client'])
-		print(" - filename : "+ args.filename)
-		print(" - retention :"+ str(args.retention))
-		print(" - namespace : "+ config['AccountClientNamespace'])
-		print(" - endpoint : "+ config['awsEndpointUrl'])
-		input("Press Enter to continue...")
-		fonctions.putRetention(args.container,args.filename,args.retention)
+		if not args.container:
+			print("The function putRetention needs a container argument to work. Please try again.")
+		elif not args.filename:
+			print("The function putRetention needs a filename argument to work. Please try again.")
+		elif not args.retention:
+			print("The function putRetention needs a retention argument to work. Please try again.")
+		else:
+			print("Running the function putRetention with the parameters below :")
+			print(" - container : "+ args.container)
+			print(" - client : "+ config['client'])
+			print(" - filename : "+ args.filename)
+			print(" - retention :"+ str(args.retention))
+			print(" - namespace : "+ config['AccountClientNamespace'])
+			print(" - endpoint : "+ config['awsEndpointUrl'])
+			input("Press Enter to continue...")
+			fonctions.putRetention(args.container,args.filename,args.retention)
 
 
 	elif(args.method.length == 0): #error handling: no method argument
@@ -236,7 +294,8 @@ else:
 		print("- add : to add a specific file in a container")
 		print("- delete : to delete a specific file inside a container")
 		print("- copy : to copy an entire folder inside a container")
-		print("- list : to list every items inside a container for a given period")
+		print("- list_containers : to list all containers")
+		print("- list_objects : to list every items inside a container for a given period")
 		print("- retrieve : to get every file from a container outside the container")
 		print("- elastic : to put every file from an elastic index inside the container")
 		print("- create_container : to create an empty container")
